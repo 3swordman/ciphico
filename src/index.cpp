4e810870eb6aa1109c64db43e152cb1977b5b820
3swordman <yyuxiaoran@qq.com>
2021-07-20 21:30:49 +0800

    add lexer with less bugs

--- src/index.cpp
+++ src/index.cpp
@@ -2,5 +2,12 @@
 #include "lexer.cpp"
 
 int main(int argc, char *argv[]) {
-    
+    if (argc == 2) {
+        auto file = std::fopen(argv[1], "r");
+        std::list<std::string> lexer_content;
+        lexer::parse(lexer_content, file);
+        for (auto&& i : lexer_content) {
+            std::cout << i << " ";
+        }
+    }
 }
