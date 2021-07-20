fa0cda04cccca12beed6f6f44a6c939a507ffc2b
3swordman <yyuxiaoran@qq.com>
2021-07-20 21:48:50 +0800

    docs: add comment and docs

--- src/base.cpp
+++ src/base.cpp
@@ -15,8 +15,20 @@
 
 #include "is_something.cpp"
 static const std::string EOL = "..";
+/**
+ * @brief Print error message and quit, the exit code is 1
+ * 
+ * @param str error message
+ */
 [[ noreturn ]] void make_lexer_error(const std::string& str) {
     std::cerr << str << std::endl;
     std::exit(1);
 }
+#if defined(__GNUC__) || defined(__clang__)
+#define expect_false(things) __builtin_expect(!!(things), 0)
+#define expect_true(things) __builtin_expect(!!(things), 1)
+#else
+#define expect_false(things) (!!(things))
+#define expect_true(things) (!!(things))
+#endif
 #endif
