4e76ee5e7e286b9f83681d875caad123be772e0b
3swordman <yyuxiaoran@qq.com>
2021-07-20 21:16:21 +0800

    feat: add lexer

--- src/base.cpp
+++ src/base.cpp
@@ -1,8 +1,16 @@
+#pragma once
+#ifndef BASE_CPP
+#define BASE_CPP
 #include <iostream>
 #include <list>
 #include <vector>
 #include <deque>
 #include <string>
+#include <unordered_set>
+
 #include <cstddef>
 #include <cstdlib>
 #include <cstdio>
+
+#include "is_something.cpp"
+#endif
