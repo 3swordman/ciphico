2c96d435d2b845a68f7dfd4d9e31ac6f6b936f5d
3swordman <yyuxiaoran@qq.com>
2021-07-20 21:48:50 +0800

    add comment and docs

--- src/is_something.cpp
+++ src/is_something.cpp
@@ -4,7 +4,10 @@
 #include <string>
 #include <unordered_set>
 namespace is_something_datas {
-    static std::unordered_set<std::string> keyword_list {
+    /**
+     * @brief A list which contains keywords
+     */
+    static const std::unordered_set<std::string> keyword_list {
         "if",
         "while",
         "else",
@@ -16,8 +19,10 @@ namespace is_something_datas {
         "or"
     };
 
-    // emmm, it is something useful
-    static std::unordered_set<std::string> truely_operator_list {
+    /**
+     * @brief A list which contains operators
+     */
+    static const std::unordered_set<std::string> truely_operator_list {
         ".",
         ",",
         "%",
@@ -44,6 +49,10 @@ namespace is_something_datas {
         "*=",
         "/="
     };
+
+    /**
+     * @brief A list which contains things maybe operator, maybe a part of operator
+     */
     static std::unordered_set<char> maybe_operator_list {
         '+',
         '-',
@@ -58,21 +67,44 @@ namespace is_something_datas {
     };
 
 };
-
+/**
+ * @brief Tell you if the arg is a keyword
+ * 
+ * @param keyword_string Something you don't sure if it is a keyword
+ * @return true The arg is a keyword
+ * @return false The arg isn't a keyword
+ */
 bool is_keyword(const std::string& keyword_string) noexcept {
     return is_something_datas::keyword_list.find(keyword_string) != is_something_datas::keyword_list.end();
 }
 
-
-
+/**
+ * @brief Tell you if the arg is an operator
+ * 
+ * @param character A character you don't sure if it is an operator
+ * @return true The arg is an operator
+ * @return false The arg might not be an operator
+ */
 bool is_operator(char character) noexcept {
     return is_something_datas::truely_operator_list.find(std::string("") + character) != is_something_datas::truely_operator_list.end();
 }
-
+/**
+ * @brief Tell you if the arg is an operator
+ * 
+ * @param str A string you don't sure if it is an operator
+ * @return true The arg is an operator
+ * @return false The arg might not be an operator
+ */
 bool is_operator(const std::string& str) noexcept {
     return is_something_datas::truely_operator_list.find(str) != is_something_datas::truely_operator_list.end();
 }
-
+/**
+ * @brief Tell you if the arg might be an operator
+ * 
+ * @param character 
+ * @return true The arg might be a operator
+ * @return false The arg isn't a operator
+ */
 bool maybe_operator(char character) noexcept {
     return is_something_datas::maybe_operator_list.find(character) != is_something_datas::maybe_operator_list.end() || is_operator(character);
 }
--- src/lexer.cpp
+++ src/lexer.cpp
@@ -4,6 +4,9 @@
 #include "base.cpp"
 #include "is_something.cpp"
 namespace lexer {
+    /**
+     * @brief The state of parseing content
+     */
     enum class char_type : unsigned char {
         unknown = 0,
         word = 1,
@@ -13,6 +16,13 @@ namespace lexer {
         comment = 5
     };
 
+    /**
+     * @brief Parse the file to a list
+     * 
+     * @param lexer_content The output of this function
+     * @param file The phico source file
+     * @exception nothing When there are some error, the function will call make_lexer_error, and the program's exit code will be 1
+     */
     void parse(std::list<std::string>& lexer_content, std::FILE *file) noexcept {
         char_type type_of_i = char_type::unknown;
         std::string word = "";
@@ -20,9 +30,10 @@ namespace lexer {
         char i;
         bool has_more_char{};
         while (true) {
-            if (!has_more_char) {
+            // For an example, the content of file is "test", the i will be "t", "e", "s", "t" and " "
+            if (expect_true(!has_more_char)) {
                 _i = std::fgetc(file);
-                if (_i == EOF) {
+                if (expect_false(_i == EOF)) {
                     has_more_char = true;
                     _i = ' ';
                 }
@@ -30,67 +41,101 @@ namespace lexer {
             } else {
                 break;
             }
+            // Main code
             restart:
             switch (type_of_i) {
+                case char_type::unknown:
+                    if (i == '#') {
+                        // The variable i is '#', it means there are some comment after it
+                        type_of_i = char_type::comment;
+                    } else if (is_operator(i)) {
+                        // The variable i is an operator, it means there are some other thing like number and string after it
+                        // We don't know what is it, so type_of_i is still char_type::unknown
+                        std::string temp;
+                        temp += i;
+                        lexer_content.push_back(std::move(temp));
+                    } else if (maybe_operator(i)) {
+                        // The variable i might be an operator, might be a part of operator
+                        type_of_i = char_type::symbol;
+                        word += i;
+                    } else if (std::isdigit(i)) {
+                        // The variable i is a digit, there are also some digit after it
+                        type_of_i = char_type::number;
+                        word += i;
+                    } else if (i == '"') {
+                        // The variable i is the beginning of a string
+                        type_of_i = char_type::string;
+                        word += i;
+                    } else if (i == ' ' || i == '\r') {
+                        // The variable i is some useless thing like space
+                        break;
+                    } else if (i == '\n') {
+                        // The variable i is the end of line
+                        lexer_content.push_back(EOL);
+                    } else {
+                        // The variable i is a word, the content of it may be a function or a variable
+                        type_of_i = char_type::word;
+                        word += i;
+                    } 
+                    break;
                 case char_type::comment:
+                    // It's the comment, so we ignore it
                     if (i == '\n') {
                         type_of_i = char_type::unknown;
                         goto restart;
                     }
                     break;
                 case char_type::number:
-                    if (!isdigit(i)) {
+                    if (!std::isdigit(i)) {
+                        // It isn't the digit, it's the end of the number
                         lexer_content.push_back(std::move(word));
                         word = "";
                         type_of_i = char_type::unknown;
                         goto restart;
                     } else {
+                        // The variable i is a digit, so put it into the word
                         word += i;
                     }
                     break;
-                    // oh, this character isn't digit, maybe there is a operator
                 case char_type::string:
                     if (i == '"') {
-                        // end of a string
+                        // It is the end of a string
                         lexer_content.push_back(std::move(word) + "\"");
                         word = "";
                         type_of_i = char_type::unknown;
                     } else {
-                        // still in a string
+                        // It is the part of the string
                         word += i;
                     }
                     break;
                 case char_type::symbol:
                     if (i == '\n') { 
-                        // there is a newline between the words
+                        // There is a newline between the words
                         lexer_content.push_back(std::move(word));
                         word = "";
                         type_of_i = char_type::unknown;
                         goto restart;
                     } else if (i == ' ') {
-                        // there is a space between the symbols
+                        // There is a space between the symbols
                         lexer_content.push_back(std::move(word));
                         word = "";
                         type_of_i = char_type::unknown;
                         break;
                     } else if (i == '"') {
-                        // there is a space between the symbols
+                        // There is a space between the symbols
                         lexer_content.push_back(std::move(word));
                         word = "";
                         type_of_i = char_type::unknown;
                         goto restart;
                     } else if (is_operator(word + i)) {
+                        // this is a operator
                         word += i;
                         lexer_content.push_back(std::move(word));
                         word = "";
                         type_of_i = char_type::unknown;
                         break;
-                    } else if (isdigit(i) || isalpha(i)) {
-                        lexer_content.push_back(std::move(word));
-                        word = "";
-                        type_of_i = char_type::unknown;
-                        goto restart;
-                    } else if (is_operator(word)) {
+                    } else if (std::isdigit(i) || std::isalpha(i)) {
+                        // the beginning of it is an operator
                         lexer_content.push_back(std::move(word));
                         word = "";
                         type_of_i = char_type::unknown;
@@ -100,54 +145,26 @@ namespace lexer {
                     }
                 case char_type::word:
                     if (i == '\n') {
-                        // there is a space between the words
+                        // It is the end of a word
                         lexer_content.push_back(std::move(word));
                         word = "";
                         type_of_i = char_type::unknown;
                         goto restart;
                     } else if (i == ' ') {
+                        // It is the end of a word
                         lexer_content.push_back(std::move(word));
                         word = "";
                         type_of_i = char_type::unknown;
                         break;
                     } else if (maybe_operator(i)) {
-                        // end of a word
+                        // It is the end of a word
                         lexer_content.push_back(std::move(word));
                         word = "";
                         type_of_i = char_type::unknown;
                         goto restart;
                     } else {
+                        // It is still a part of the worf
                         word += i;
-                    }
-                    break;
-                case char_type::unknown:
-                    if (i == '#') {
-                        type_of_i = char_type::comment;
-                    } else if (is_operator(i)) {
-                        std::string temp;
-                        temp += i;
-                        lexer_content.push_back(std::move(temp));
-                    } else if (maybe_operator(i)) {
-                        word += i;
-                        type_of_i = char_type::symbol;
-                    } else if (std::isdigit(i)) {
-                        // i is a digit
-                        if (type_of_i == char_type::unknown) {
-                            type_of_i = char_type::number;
-                        }
-                        word += i;
-                        // wait until next letter is not digit
-                    } else if (i == '"') {
-                        // begin of a string
-                        word += "\"";
-                        type_of_i = char_type::string;
-                    } else if (i == ' ' || i == '\r') {
-                        break;
-                    } else if (i == '\n') {
-                        lexer_content.push_back(EOL);
-                    } else {
-                        word += i;
-                        type_of_i = char_type::word;
                     }
                     break;
             }
