8d31dfc3c33261754e80d3426a24d8c39c1ecd8f
老子就叫Your Name <you@example.com>
2021-07-31 21:36:37 +0800

    fix: fix bug that the program cannot be compiled

---
+++ CMakeFiles/ciphico.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ciphico.dir/src/index.cpp.o"
+  "ciphico"
+  "ciphico.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ciphico.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
